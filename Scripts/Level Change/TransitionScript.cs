fe5083273fbee5b8a11e42a80f5bac3bc91731ee
zoshaikh17 <55362861+zoshaikh17@users.noreply.github.com>
2020-11-22 18:44:43 -0600

    Add files via upload

---
+++ Scripts/Level Change/TransitionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionScript : MonoBehaviour
+{
+    public string levelToLoad;
+    private float timer = 2f;
+    private Text timerSeconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f2");
+        if (timer <= 0)
+        {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+}
